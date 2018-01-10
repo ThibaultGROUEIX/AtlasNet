fb546b8d6f711e110ac7bc86f85f8f3563c8b1c0
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2018-01-10 20:10:41 +0100

    license

--- run.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-gpu_id=0
-env=AE_AtlasNet
-nb_primitives=25
-source activate pytorch-sources
-. switch-gpu $gpu_id
-git pull
-python ./training/train_AE_AtlasNet.py --env $env  |& tee ${env}.txt
-FreeSms.py "code finished or bugged ${env} on ${HOSTNAME}"
