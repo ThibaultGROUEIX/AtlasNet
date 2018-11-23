b74f3dedc155fcb7cd9795d655913bbf7be52857
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2018-11-23 14:04:52 +0100

    download script

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,3 +1,16 @@
 #This script download the data from ENPC cloud
-wget data --no-check-certificate
-unzip
+
+# The point clouds from ShapeNet, with normals
+wget https://cloud.enpc.fr/s/j2ECcKleA1IKNzk/download --no-check-certificate
+unzip download
+rm download
+
+# The corresponding normalized mesh (for the metro distance)
+wget https://cloud.enpc.fr/s/RATKsfLQUSu0JWW/download --no-check-certificate
+unzip download
+rm download
+
+# the rendered views
+wget https://cloud.enpc.fr/s/S6TCx1QJzviNHq0/download --no-check-certificate
+unzip download
+rm download
