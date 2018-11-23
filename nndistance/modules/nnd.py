f803ca89132e9a856957d4297c28b6b1e625c738
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2018-11-23 13:38:54 +0100

    migration pytorch v1

--- nndistance/modules/nnd.py
+++
@@ -1,6 +0,0 @@
-from torch.nn.modules.module import Module
-from functions.nnd import NNDFunction
-
-class NNDModule(Module):
-    def forward(self, input1, input2):
-        return NNDFunction()(input1, input2)
